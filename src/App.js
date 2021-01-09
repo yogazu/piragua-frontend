004e1a4b25189d16935a99c667d85fbf5e279592
yogazu <yovannygalvez@gmail.com>
2021-01-09 14:48:19 -0500

    Clasde 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Fragment } from "react";
 import React from "react";
 import "./styles/styles.scss";
-import Piragua from './Piragua'
-
+import Piragua from './Piragua';
+import Banner from './Banner';
+import Formulario from './Formulario'
 
 const datos = [ 
   {
@@ -33,23 +34,8 @@ const datos = [
 
 const App = () => (
   <Fragment>
-  <div className="main-banner img-container l-section" id="main-banner">
-  <div className="ed-grid lg-grid-6">
-    <div className="lg-cols-4 lg-x-2">
-      <img className="main-banner__img" alt = "banner"  src="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=60" />
-      <div className="main-banner__data s-center">
-        <p className="t2 s-mb-0">Proyecto Piragua</p>
-        <p> Informes</p>
-        <a href="https://ed.team/" className="button">Ingresar</a>
-      </div>
-    </div>
-  </div>
-</div>
-<div className="ed-grid m-grid-3">
-  {
-    datos.map(d => <Piragua title={d.title} image={d.image} price={d.price} id={d.id}/>)
-  }
-</div>
+  <Banner/>
+  <Formulario/>
 </Fragment>
 
 )
---
+++ src/Formulario.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+
+class Formulario extends React.Component {
+
+    constructor (props){
+        super(props)
+
+        this.state = 
+            {
+                nombre  :"",
+                correo :""
+            }
+            this.cambiarNombre = this.cambiarNombre.bind(this);
+            this.cambiarCorreo = this.cambiarCorreo.bind(this);
+
+        }
+        cambiarNombre(e) {
+              this.setState({
+                  nombre : e.target.value
+              })  
+
+        }
+        cambiarCorreo(e){
+            this.setState({
+                correo: e.target.value
+            })
+        }
+        //this.setState({})
+
+    render() {
+        
+        return (
+            <div className="ed-grid">
+                <h1>FORMULARIO</h1>
+                <form id="elemento">
+                    <div className="ed-grid m-grid-2">
+                        <div className="form__item">
+                            <label >Nombre completo</label>
+                            <input type="text" onChange={e => this.setState({
+                                nombre: e.target.value
+                            })}/>
+                        </div>
+                        <div className="form__item">
+                            <label >Correo electrónico</label>
+                            <input type="email"
+                                onChange={this.cambiarCorreo}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <h2>{`Hola ${this.state.nombre}`}</h2>
+                        <span>{`Tu correo es: ${this.state.correo}`}</span>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+    componentDidMount () {
+        
+        let elemento = document.getElementById("elemento")
+        console.log (elemento)
+
+    }
+
+    componentDidUpdate (prevProps, prevState) {
+        console.log (prevProps)
+        console.log (prevState)
+        console.log ("--------")
+
+    }
+
+    componentWillUnmount () {
+        
+    }
+
+}
+
+export default Formulario
